2199bdf1c033649661bf678b6d5d245dec4cf2cd
Neko Gong <g405252865@163.com>
2024-06-29 21:40:12 +0800

    add cors, plugin

--- index.mjs
+++ index.mjs
@@ -11,6 +11,29 @@ import FileRsp from './FileRsp.mjs'
 export { FileRsp }
 
 const handler = async (req, res) => {
+    if (settings.plugin(req, res)) {
+        return
+    }
+
+    if (settings.cors.origins) {
+        res.setHeader('Access-Control-Allow-Origin', settings.cors.origins)
+    }
+    if (settings.cors.methods) {
+        res.setHeader('Access-Control-Allow-Methods', settings.cors.methods)
+    }
+    if (settings.cors.headers) {
+        res.setHeader('Access-Control-Allow-Headers', settings.cors.headers)
+    }
+    if (settings.cors.credential) {
+        res.setHeader('Access-Control-Allow-Credentials', 'true')
+    }
+
+    if (req.method === 'OPTIONS' && settings.cors.origins) {
+        res.statusCode = 200
+        res.end()
+        return
+    }
+
     const ctx = {
         state: {}
     }
@@ -56,11 +79,30 @@ const handler = async (req, res) => {
 }
 
 const settings = {
+    cors: {
+        origins: null,
+        methods: null,
+        headers: null,
+        credential: false
+    },
+    plugin: () => {},
+    protocol: null,
     authHandler: null,
     endHandler: null,
     errorHandler: null,
     rpcFolder: null,
     staticFolder: null
+}
+
+const useProtocol = protocol => {
+    if (protocol !== 'http' && protocol !== 'https' && protocol !== null) {
+        throw new Error('Invalid protocol')
+    }
+    settings.protocol = protocol
+}
+
+const usePlugin = plugin => {
+    settings.plugin = plugin
 }
 
 const useAuth = handler => {
@@ -83,22 +125,50 @@ const useStatic = folder => {
     settings.staticFolder = resolve(folder)
 }
 
-export const createServer = options => {
+const useCors = (options = '*') => {
+    if (typeof options === 'string') {
+        options = {
+            origins: options
+        }
+    }
     const {
-        port = 8080,
-        hostname = '0.0.0.0',
-        key = null,
-        cert = null
-    } = { ...options }
+        origins = '*',
+        methods = '*',
+        headers = 'Content-Type, Authorization, Pragma, Cache-Control, X-Requested-With, X-Simple-Rpc-Params',
+        credential = true
+    } = options
+    settings.cors.origins = origins
+    settings.cors.origins = origins
+    settings.cors.methods = methods
+    settings.cors.headers = headers
+    settings.cors.credential = credential
+}
 
-    const useHttps = key && cert
-    const module = useHttps ? https : http
+export const createServer = options => {
+    options = { ...options }
+    if (!options.port) {
+        options.port = 8080
+    }
 
-    const server = module.createServer(handler, options)
+    let protocol = null
+    if (!settings.protocol) {
+        const { key = null, cert = null } = options
+        const useHttps = key && cert
+        protocol = useHttps ? https : http
+    } else if (settings.protocol === 'http') {
+        protocol = http
+    } else if (settings.protocol === 'https') {
+        protocol = https
+    }
+
+    const server = protocol.createServer(handler, options)
 
     return {
         server,
         options,
+        useProtocol,
+        usePlugin,
+        useCors,
         useAuth,
         useEnd,
         useError,
@@ -108,7 +178,7 @@ export const createServer = options => {
         start: async () => {
             return new Promise((resolve, reject) => {
                 server.on('error', e => reject(e))
-                server.listen(port, hostname, resolve)
+                server.listen(options, resolve)
             })
         }
     }
